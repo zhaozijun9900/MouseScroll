24dd490882bb972cc0c9fbaa10fe181e800a79a3
zhaozijun9900 <80299517@qq.com>
2017-10-12 18:14:39 +0800

    5

--- 2-index.js
+++ 2-index.js
@@ -1,32 +1,32 @@
 //拖拽函数
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 var box = document.querySelector("div");
-=======
+
 box.addEventListener("mousedown", down);
 
 function down(ev) {
 	var disX = ev.clientX - box.offsetLeft;
 	var disY = ev.clientY - box.offsetTop;
-}
->>>>>>> down
-=======
-document.addEventListener("mousemove", move);
+	
+	document.addEventListener("mousemove", move);
 
-function move(ev) {
-	var x = ev.clientX - disX;
-	var y = ev.clientY - disY;
-
-	box.style.left = x + "px";
-	box.style.top = y + "px";
-}
->>>>>>> move
-=======
-document.addEventListener("mouseup",up);
-
-function up() {
-	document.addEventListener("mousemove",move);
+	function move(ev) {
+		var x = ev.clientX - disX;
+		var y = ev.clientY - disY;
+	
+		box.style.left = x + "px";
+		box.style.top = y + "px";
+	}
+	
 	document.addEventListener("mouseup",up);
+
+	function up() {
+		document.addEventListener("mousemove",move);
+		document.addEventListener("mouseup",up);
+	}
+
 }
->>>>>>> up
+
+
+
+
