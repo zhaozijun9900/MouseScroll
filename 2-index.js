0fe7df6358d0b39b0175dfad6e1f614ad83cc4c2
zhaozijun9900 <80299517@qq.com>
2017-10-12 18:23:17 +0800

    6

--- 2-index.js
+++ 2-index.js
@@ -2,31 +2,28 @@
 
 var box = document.querySelector("div");
 
+
 box.addEventListener("mousedown", down);
 
 function down(ev) {
 	var disX = ev.clientX - box.offsetLeft;
 	var disY = ev.clientY - box.offsetTop;
-	
+
 	document.addEventListener("mousemove", move);
 
 	function move(ev) {
 		var x = ev.clientX - disX;
 		var y = ev.clientY - disY;
-	
+		console.log(box)
 		box.style.left = x + "px";
 		box.style.top = y + "px";
 	}
-	
-	document.addEventListener("mouseup",up);
+
+	document.addEventListener("mouseup", up);
 
 	function up() {
-		document.addEventListener("mousemove",move);
-		document.addEventListener("mouseup",up);
+		document.addEventListener("mousemove", move);
+		document.addEventListener("mouseup", up);
 	}
 
 }
-
-
-
-
